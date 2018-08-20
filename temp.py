a737d81e32a4c89710acac0d08077cb78b5d678f
Ginobilium-pc <Ginobilium@pc.com>
2018-08-20 17:45:29 +0800

    test

--- temp.py
+++ temp.py
@@ -7,10 +7,21 @@ This is a temporary script file.
 
 
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
-date_object = datetime.now()
+now = datetime.now()
 # convert object to the format we want
-today = date_object.strftime('%m_%d')
+today = now.strftime('%m_%d')
 today += ".md"
-file_list = os.listdir("/home/neo/10kWords")
+filelist = os.listdir("/home/neo/10kWords")
+deltalist = [1,2,4,7,15,30]
+
+with open("/home/neo/10kWords/README.md",'w') as f:
+    f.write("# 10kWords\n")
+    f.write("曲根万词班笔记\n\n")
+    for d in deltalist:
+        date =  now - timedelta(days = d)
+        filename = date.strftime('%m_%d')+".md"
+        if filename in filelist:
+            text = date.strftime("[%b. %d]")
+            f.write(text+"(\\"+filename+")"+"\n")
